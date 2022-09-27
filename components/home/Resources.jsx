41a6e75226c05a804fbed91d972debbd53f14bf1
hellodaolens <113350882+hellodaolens@users.noreply.github.com>
2022-09-28 05:24:20 +0530

    bug fix

--- components/home/Resources.jsx
+++ components/home/Resources.jsx
@@ -32,6 +32,9 @@ const Resources = ({ DAOResourcesHeading, DAOResources }) => {
 
   const [resources, setResources] = useState(DAOResources);
   const [isMobile, setIsMobile] = useState(false);
+  
+  const [readMore, setReadMore] = useState(false);
+
 
   const handleFilterResources = (type) => {
     if (type === 'all') {
@@ -106,10 +109,9 @@ const Resources = ({ DAOResourcesHeading, DAOResources }) => {
             keyBoardControl={true}
           >
             {resources?.map((resource) => {
-              const [readMore, setReadMore] = useState(false);
-              const { name, description } = resource;
-              const showMoreBtn = description?.length > 130;
               return (
+                  const { name, description } = resource;
+                  const showMoreBtn = description?.length > 130;
                 <article key={resource?.id} className="resource">
                   <Image
                     src={resource?.logo?.data?.attributes?.url}
