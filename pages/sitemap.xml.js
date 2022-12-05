396b11e83816bf1061e518e5e6d8a8e71c5847f9
siemen <siemensubbaiah1@gmail.com>
2022-12-05 17:04:39 +0530

    added sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,61 @@
+//pages/sitemap.xml.js
+const EXTERNAL_DATA_URL =
+  'https://daolens-strapi-dxrba.ondigitalocean.app/api/articles?pagination[start]=0&pagination[limit]=100';
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>https://www.daolens.com</loc>
+     </url>
+     <url>
+       <loc>https://www.daolens.com/careers</loc>
+     </url>
+     <url>
+       <loc>https://www.daolens.com/community</loc>
+     </url>
+     <url>
+       <loc>https://www.daolens.com/contribution-tool</loc>
+     </url>
+     <url>
+       <loc>https://www.daolens.com/onboarding-tool</loc>
+     </url>
+     <url>
+       <loc>https://www.daolens.com/blog</loc>
+     </url>
+          ${posts?.data
+            .map((post) => {
+              return `
+       <url>
+           <loc>${`https://www.daolens.com/blog/${post?.attributes?.slug}`}</loc>
+       </url>
+     `;
+            })
+            .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps = async ({ res }) => {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const posts = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default SiteMap;
